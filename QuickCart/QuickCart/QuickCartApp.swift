083230434960394fd3d65ca030255ade52017977
Akshat Gulati <103839527+akshat-gulati@users.noreply.github.com>
2024-08-06 23:28:09 +0530

    First Commit

---
+++ QuickCart/QuickCart/QuickCartApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickCartApp.swift
+//  QuickCart
+//
+//  Created by Akshat Gulati on 06/08/24.
+//
+
+import SwiftUI
+
+@main
+struct QuickCartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
